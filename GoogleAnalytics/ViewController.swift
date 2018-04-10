70d7dcac721d03b156dd2307cc22cc0dcf0414e3
andrew54068 <andrew0424718012@yahoo.com.tw>
2018-04-10 17:54:25 +0800

    Initial Commit

---
+++ GoogleAnalytics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleAnalytics
+//
+//  Created by kidnapper on 10/04/2018.
+//  Copyright © 2018 25sprout.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
