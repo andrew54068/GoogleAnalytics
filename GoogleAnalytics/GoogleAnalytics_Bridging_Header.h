6eae0d771d715a9b30f61c9969eb8db9645d2e84
andrew54068 <andrew0424718012@yahoo.com.tw>
2018-04-13 00:01:16 +0800

    [feat] example for GoogleAnalytics event and timing. see event in "REAL-TIME" tab and see timing in "BEHAVIOR" -> "App Speed" tab on GoogleAnalytics console website

---
+++ GoogleAnalytics/GoogleAnalytics_Bridging_Header.h
@@ -0,0 +1,13 @@
+//
+//  GoogleAnalytics-Bridging-Header.h
+//  GoogleAnalytics
+//
+//  Created by kidnapper on 10/04/2018.
+//  Copyright © 2018 25sprout.com. All rights reserved.
+//
+#import <Google/Analytics.h>
+//#ifndef GoogleAnalytics_Bridging_Header_h
+//#define GoogleAnalytics_Bridging_Header_h
+//
+//#endif /* GoogleAnalytics_Bridging_Header_h */
+
--- GoogleAnalytics/ViewController.swift
+++ GoogleAnalytics/ViewController.swift
@@ -9,6 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var startTiming: UIButton!
+    @IBOutlet var stopTiming: UIButton!
+    @IBOutlet var sendEvent: UIButton!
+    
+    @IBAction func startTimingAction(_ sender: Any) {
+        startReadingTime = Date()
+    }
+    @IBAction func stopTimingAction(_ sender: Any) {
+        recordReadingTime()
+    }
+    @IBAction func sendEventAction(_ sender: Any) {
+        sendEventToGA()
+    }
+    
+    // info for Google analytics
+    let name = "testing name"
+    let label = "testing label"
+    
+    // Google analytics
+    var startReadingTime: Date!
+    var finishReadingTime: Date!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +40,37 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // handle GA when app entering background and active
+        NotificationCenter.default.addObserver(self, selector: #selector(recordReadingTime), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(recordReadingTime), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIApplicationWillResignActive, object: nil)
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIApplicationDidBecomeActive, object: nil)
+    }
+    
+    @objc func recordReadingTime() {
+        if startReadingTime != nil {
+            finishReadingTime = Date()
+            let readingTime = (finishReadingTime.timeIntervalSinceNow - startReadingTime.timeIntervalSinceNow) * 1000
+            let GAIDic = GAIDictionaryBuilder.createTiming(withCategory: "Reading time for example", interval: Int(readingTime) as NSNumber, name: name, label: label).build() as! [AnyHashable : Any]!
+            GAI.sharedInstance().defaultTracker.send(GAIDic)
+            startReadingTime = nil
+        }
+        else {
+            startReadingTime = Date()
+        }
+    }
+    
+    func sendEventToGA() {
+        let GAIDic = GAIDictionaryBuilder.createEvent(withCategory: name, action: "Send", label: label, value: nil).build() as! [AnyHashable : Any]!
+        GAI.sharedInstance().defaultTracker.send(GAIDic)
     }
 
 
